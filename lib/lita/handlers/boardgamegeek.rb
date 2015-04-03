cd34e2af81256870b37cd6980cd3adc985f2736b
Chris Chalfant <cchalfant@leafsoftwaresolutions.com>
2015-04-03 18:05:29 -0400

    added vagrantfile

--- lib/lita/handlers/boardgamegeek.rb
+++ lib/lita/handlers/boardgamegeek.rb
@@ -1,10 +1,20 @@
+require 'bgg-api'
+
 module Lita
   module Handlers
     class Boardgamegeek < Handler
       route(/bgg(?: me)?\s+(.+)/, :search, command: true)
 
       def search(response)
+        term = response.match_data[1]
+        id = game_id(term)
+        response.reply "#{term}: http://www.boardgamegeek.com/boardgame/#{id}"
+      end
 
+      def game_id(name)
+        bgg = BggApi.new
+        result = bgg.search(query: name, type: 'boardgame')
+        result["item"].first["id"]
       end
     end
 
--- spec/lita/handlers/boardgamegeek_spec.rb
+++ spec/lita/handlers/boardgamegeek_spec.rb
@@ -2,12 +2,18 @@ require "spec_helper"
 
 describe Lita::Handlers::Boardgamegeek, lita_handler: true do
 
-  it "routes bgg me agricola" do
-    expect(subject).to route_command("bgg me agricola").to(:search)
-  end
+  it { is_expected.to route_command("bgg me agricola").to(:search) }
+  it { is_expected.to route_command("bgg agricola").to(:search) }
 
-  it "routes bgg agricola" do
-    expect(subject).to route_command("bgg agricola").to(:search)
+  describe '#search' do
+    before(:each) do
+      allow(subject).to receive(:game_id).and_return(1)
+    end
+
+    it "replies with the search term" do
+      send_command("bgg me agricola")
+      expect(replies.first).to eq("agricola: http://www.boardgamegeek.com/boardgame/1")
+    end
   end
 
 end
